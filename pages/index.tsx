8e57c81fc7331517a40ae025443b3e4f4cb04e74
Brandon.DeVore <Brandon.DeVore@publix.com>
2022-12-01 15:19:25 -0500

    Header Change

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,14 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Next Step Discipleship/</title>
+        <title>Next Step Discipleship</title>
       </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+        Next Step Discipleship
+        </h1>
+      </main>
     </div>
   )
 }
